db3676a8d9abb2420780be821e02f35a60a0cdb8
zifu10 <zifu1051@gmail.com>
2024-02-23 08:37:24 +0700

    Initial Commit

--- TugasPemLan/HelloWord/src/App.java
+++ TugasPemLan/HelloWord/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        for(int i = 0; i < 5; i++) {
+            System.out.println("Ilman");
+        }
     }
 }
